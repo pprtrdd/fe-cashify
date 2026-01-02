f28c07088157aeaa9fa81a914154fe2f27ec650a
pprtrdd <paulr812@gmail.com>
2026-01-01 21:18:04 -0300

    feat: Integrate dotenv for environment variables and remove Firebase options file

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:cashify/features/transaction/presentation/providers/movement_pro
 import 'package:cashify/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
+  
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
