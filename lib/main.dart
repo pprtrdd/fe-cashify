7928fc3387bd6d20d98c5229dc77cbb33c33db02
pprtrdd <paulr812@gmail.com>
2026-01-07 14:25:51 -0300

    refactor: Implement dependency injection using get_it; streamline MovementProvider initialization and remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:cashify/core/auth/auth_wrapper.dart';
-import 'package:cashify/features/transaction/domain/repositories/category_repository.dart';
-import 'package:cashify/features/transaction/domain/repositories/movement_repository.dart';
-import 'package:cashify/features/transaction/domain/repositories/payment_method_repository.dart';
-import 'package:cashify/features/transaction/domain/usecases/category_usecases.dart';
-import 'package:cashify/features/transaction/domain/usecases/movement_usecases.dart';
-import 'package:cashify/features/transaction/domain/usecases/payment_method_usecases.dart';
 import 'package:cashify/features/transaction/presentation/providers/movement_provider.dart';
 import 'package:cashify/firebase_options.dart';
+import 'package:cashify/injection_container.dart' as di;
+import 'package:cashify/injection_container.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -18,24 +14,12 @@ void main() async {
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  /* TODO: Buscar una mejor forma de importar providers (pueden crecer mucho) */
+  await di.init();
 
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => MovementProvider(
-            movementUseCase: MovementUseCase(
-              movementRepository: MovementRepository(),
-            ),
-            categoryUsecases: CategoryUsecases(
-              categoryRepository: CategoryRepository(),
-            ),
-            paymentMethodUsecases: PaymentMethodUsecases(
-              paymentMethodRepository: PaymentMethodRepository(),
-            ),
-          ),
-        ),
+        ChangeNotifierProvider(create: (_) => sl<MovementProvider>()),
       ],
       child: const MainApp(),
     ),
