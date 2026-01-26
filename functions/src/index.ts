3e6d32df63d19a35d0a2146d4be29db284a1b86e
pprtrdd <paulr812@gmail.com>
2026-01-26 16:47:36 -0300

    added defaultCategories and defaultConfig to cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,4 +2,6 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export { seedAppConfig } from "./seed/appConfig";
+export { seedDefaultCategories } from "./seed/defaultCategories";
+export { seedDefaultConfig } from "./seed/defaultConfig";
 export { seedPaymentMethods } from "./seed/paymentMethods";
