8eb2096e5bd0c93c1a52ca259388462f52c1d921
pprtrdd <paulr812@gmail.com>
2026-01-27 00:58:16 -0300

    added categories seed to all user categories updated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
+export { migrateAllUserCategoriesFromTemplate } from "./seed/categories";
 export { seedAppConfig } from "./seed/appConfig";
 export { seedDefaultCategories } from "./seed/defaultCategories";
 export { seedDefaultConfig } from "./seed/defaultConfig";
